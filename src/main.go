765f9c8b3b43d0b144bb8fefde97b7a56c308801
Brandon Conway <brandoncc@gmail.com>
2025-01-01 00:48:22 -0800

    Solve day 3, part 1

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	dayonepartone "advent_of_code_2024/src/day_one/part_one"
 	dayoneparttwo "advent_of_code_2024/src/day_one/part_two"
+	daythreepartone "advent_of_code_2024/src/day_three/part_one"
 	daytwopartone "advent_of_code_2024/src/day_two/part_one"
 	daytwoparttwo "advent_of_code_2024/src/day_two/part_two"
 	"fmt"
@@ -13,4 +14,5 @@ func main() {
 	fmt.Printf("Day 1, part 2: %s\n", dayoneparttwo.Solve())
 	fmt.Printf("Day 2, part 1: %s\n", daytwopartone.Solve())
 	fmt.Printf("Day 2, part 2: %s\n", daytwoparttwo.Solve())
+	fmt.Printf("Day 3, part 1: %s\n", daythreepartone.Solve())
 }
